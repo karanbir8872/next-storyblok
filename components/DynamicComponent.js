04761c0cd27adb18f23e9f155e2bd01bfb121734
karanbir8872 <wwwkaranbir786@gmail.com>
2021-05-01 10:38:54 +0530

    add

---
+++ components/DynamicComponent.js
@@ -0,0 +1,21 @@
+import SbEditable from 'storyblok-react'
+import Teaser from './Teaser'
+
+// resolve Storyblok components to Next.js components
+const Components = {
+  'teaser': Teaser,
+}
+
+const DynamicComponent = ({blok}) => {
+  // check if component is defined above
+  // console.log(blok,"blok");
+  if (typeof Components[blok.component] !== 'undefined') {
+    const Component = Components[blok.component]
+    // wrap with SbEditable for visual editing
+    return (<SbEditable content={blok}><Component blok={blok} /></SbEditable>)
+  }
+
+  return (<p>The component <strong>{blok.component}</strong> has not been created yet.</p>)
+}
+
+export default DynamicComponent
